0c5b9f6e45c453c07d4c2dfffac20fb09fb63d63
Yong <ysk77@hotmail.com>
2014-04-09 15:45:01 -0400

    Adding feature to edit pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -13,15 +13,28 @@ class PicturesController < ApplicationController
   end
     
   def create
-    @picture = Picture.new(picture_params)
+    @picture = Picture.new(picture_params) 
     if @picture.save
-      redirect_to pictures_url
+      redirect_to "/pictures"
     else
       render :new
     end
   end
     # render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"  
-  
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @oicture = Picture.find(params[:id])
+
+    if @picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
   private
   def picture_params
     params.require(:picture).permit(:artist, :title, :url)
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,10 @@ Photogur::Application.routes.draw do
   get 'pictures/new' => 'pictures#new' 
 
   get 'pictures/:id' => 'pictures#show', as: "picture"  # ":id" is like wildcard
-
+  
+  get 'pictures/:id/edit' => 'pictures#edit', as: 'edit_picture'
+  patch 'pictures/:id' => 'pictures#update'
+  
 end
 
 
